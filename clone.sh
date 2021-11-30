510d6d645179897dd2946529da125a2c89ba799b
Paul Mullins <paulmullins@protonmail.com>
2021-11-30 19:30:11 +0000

    Updated and tested the clone script

--- clone.sh
+++ clone.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 git clone https://github.com/sourcecred/template-instance.git
-cp template-instance/* .
+mv README.md README2.md
+cp -r template-instance/* .
+mv README.md Sourcred-README.md
+mv README2.md README.md
